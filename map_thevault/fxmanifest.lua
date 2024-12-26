78ed7db826086a1523031044c9ed21613f523278
Rainbowkv <981512226@qq.com>
2024-12-26 22:36:49 +0800

    新室内场景-金库（戴维斯警局旁）

---
+++ map_thevault/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file 'TIMECYCLEMOD_FILE' 'floky_timecycle.xml'
+data_file 'AUDIO_GAMEDATA' 'floky_vaultbank_doors_sound_game.dat'
+
+files {
+  'floky_vaultbank_doors_sound_game.dat151.rel',
+  'floky_timecycle.xml',
+}
