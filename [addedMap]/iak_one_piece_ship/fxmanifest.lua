4b8e112f58bf84b1fcad9e06ac7b443c1f2c7ec7
Rainbowkv <981512226@qq.com>
2024-12-31 16:18:13 +0800

    加入11个地图；1辆轮椅

---
+++ [addedMap]/iak_one_piece_ship/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean"
+
+game "gta5"
+
+author 'Iakko'
+
+description "Going Merry - One Piece"
+
+discord "https://discord.gg/tqk3kAEr4f"
+
+this_is_a_map 'yes'
+
+--cord: -1873.65, -1293.90, 8.85
+
+
+
