d3b08e45d0804f5e3d5d44b9248d16a62083647e
Rainbowkv <981512226@qq.com>
2025-01-21 16:49:33 +0800

    减轻负载，仅请求methlab

---
+++ [ps]/tuner-methlab/client/main.lua
@@ -0,0 +1,7 @@
+CreateThread(function()
+    if GetGameBuildNumber() >= 2372 then
+        -- TunerGarage.LoadDefault() -- -1350.0, 160.0, -100.0
+        TunerMethLab.LoadDefault() -- 981.9999, -143.0, -50.0
+        -- TunerMeetup.LoadDefault() -- -2000.0, 1113.211, -25.36243
+    end
+end)
---
+++ [ps]/tuner-methlab/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'rainbow'
+description 'Load and customize your map'
+version '2.4.0'
+
+lua54 "yes"
+
+client_scripts {
+    'client/methlab.lua',
+    'client/main.lua',
+}
